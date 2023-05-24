effb426b1ced464ffd6ca891fcf20813dc59cb4a
ShehzorMemon <Shezymemon0180@gmail.com>
2023-05-24 23:00:51 +0500

    Added all the files.

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Temp from "./components/weather/temp";
+
+function App() {
+  return (
+    <div className="App">
+      <Temp />
+    </div>
+  );
+}
+
+export default App;
